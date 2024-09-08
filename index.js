927ad6bc9857967669949d9464498a1facc66b7b
Tapas Chandra <tapaskulti@gmail.com>
2024-09-09 01:09:02 +0530

    port 8080

--- index.js
+++ index.js
@@ -295,8 +295,12 @@ const generateAccessToken = async () => {
 
 const PORT = process.env.PORT || 8080;
 
-console.log(`Attempting to start server on port ${PORT}`);
-server.listen(PORT, '0.0.0.0', () => {
+// console.log(`Attempting to start server on port ${PORT}`);
+// server.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
