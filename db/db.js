31547aca9a5600d0885b9a7b69d0effdf992e329
Tapas Chandra <tapaskulti@gmail.com>
2024-11-12 02:05:27 +0530

    mongodb uri

--- db/db.js
+++ db/db.js
@@ -15,3 +15,25 @@ const connectWithDb = () => {
 };
 
 module.exports = connectWithDb;
+
+
+// const mongoose = require("mongoose");
+
+// const connectWithDb = async (req, res) => {
+//   try {
+//     mongoose.set("strictQuery", false);
+//     mongoose
+//       .connect(process.env.MONGO_URI)
+//       mongoose.connection
+//       .once("open", () => {
+//         console.log("Connected to mongodb");
+//       })
+//       .on("error", (error) => {
+//         console.log("Error connecting to mongodb", error);
+//       });
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// module.exports = connectWithDb;
