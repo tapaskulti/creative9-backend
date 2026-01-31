e144d6827448608c1fdd4b8846a3c6b831cd3785
Tapas Chandra <tapaskulti@gmail.com>
2026-02-01 00:38:59 +0530

    toast msg change

--- controllers/userController.js
+++ controllers/userController.js
@@ -277,7 +277,7 @@ exports.forgotPasswordmailxxxxxx = async (req, res, next) => {
 
     res.status(200).json({
       success: true,
-      message: "email sent succesfully",
+      message: "Email sent succesfully!",
     });
   } catch (error) {
     user.forgotPasswordToken = undefined;
